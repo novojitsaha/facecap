096c7fb75df2510dbc619c772c79cefd481b783b
Novojit Saha <n.saha@student.utwente.nl>
2025-10-19 14:37:55 +0200

    Refactor layout and index components to use SafeAreaView and FlatList for initial UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView className="bg-primary">
+        <Text className="text-center font-bold text-2xl">FaceCap</Text>
+      </SafeAreaView>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import VideoType from "./types/videoType";
+
+const data: VideoType[] = [
+  {
+    id: 1,
+    timestamp: "18-10-2025",
+  },
+  {
+    id: 2,
+    timestamp: "19-10-2025",
+  },
+  {
+    id: 3,
+    timestamp: "20-10-2025",
+  },
+];
+
+const VideoItem = ({ id, timestamp }: VideoType) => (
+  <View className="flex-row items-center border border-gray-400 rounded-xl h-24 px-4 mb-3 mx-3 bg-white">
+    <TouchableOpacity>
+      <Text>▶</Text>
+    </TouchableOpacity>
+    <Text className="flex-1 text-center">{timestamp}</Text>
+    <TouchableOpacity>
+      <Text>☰</Text>
+    </TouchableOpacity>
+  </View>
+);
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-slate-300 h-full">
+      <Text className="text-center mb-4">Total Storage: 12 MB</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <VideoItem id={item.id} timestamp={item.timestamp} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </SafeAreaView>
   );
 }
